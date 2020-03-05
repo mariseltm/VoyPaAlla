2d892afd8ac2276b7a38c96ff121572d8aac18f9
carmo <carmo@aleph.engineering>
2020-03-04 23:19:02 -0500

    Reformat style of file

--- App.js
+++ App.js
@@ -9,9 +9,9 @@ export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
 
-const configureNewGameHandler = () => {
-  setGuessRounds(0);
-};
+  const configureNewGameHandler = () => {
+    setGuessRounds(0);
+  };
 
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
@@ -23,13 +23,13 @@ const configureNewGameHandler = () => {
     setUserNumber(null);
   };
 
-  let content = <MainScreen onStartGame={startGameHandler}/>;
+  let content = <MainScreen onStartGame={startGameHandler} />;
 
   if (userNumber && guessRounds <= 0) {
     content = <GameScreen userChoice={userNumber}
-    onGameOver={gameOverHandler}/>;
+      onGameOver={gameOverHandler} />;
   } else if (guessRounds > 0) {
-    content = <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} onRestart={configureNewGameHandler}/>;
+    content = <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} onRestart={configureNewGameHandler} />;
   };
 
   return (
