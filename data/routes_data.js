0283676c7ae18bd79223c7f069f947492080bfe7
Alberto Carmona <carmo@aleph.engineering>
2020-03-16 17:00:25 -0400

    Create data for different bus lines

---
+++ data/routes_data.js
@@ -0,0 +1,166 @@
+export const routeData = [
+    {
+        routeName: 'P1',
+        stops:
+            [
+                { stopName: 'P1 stop 1', geo: [123, 123], seenInfo: 1 },
+                { stopName: 'P1 stop 2', geo: [123, 123], seenInfo: 0 },
+                { stopName: 'P1 stop 3', geo: [123, 123], seenInfo: 0.5 },
+            ]
+    },
+    {
+        routeName: 'P2',
+        stops:
+            [
+                { stopName: 'P2 stop 1', geo: [123, 123], seenInfo: 1 },
+                { stopName: 'P2 stop 2', geo: [123, 123], seenInfo: 0 },
+                { stopName: 'P2 stop 3', geo: [123, 123], seenInfo: 0.5 },
+            ]
+    },
+    {
+        routeName: 'P3',
+        stops:
+            [
+                { stopName: 'P3 stop 1', geo: [123, 123], seenInfo: 1 },
+                { stopName: 'P3 stop 2', geo: [123, 123], seenInfo: 0 },
+                { stopName: 'P3 stop 3', geo: [123, 123], seenInfo: 0.5 },
+            ]
+    },
+    {
+        routeName: 'P4',
+        stops:
+            [
+                { stopName: 'Novia del Mediodía', geo: [123, 123], seenInfo: 1 },
+                { stopName: 'Bar Madera', geo: [123, 123], seenInfo: 0 },
+                { stopName: 'Iglesia Arroyo Arenas', geo: [123, 123], seenInfo: 0.5 },
+            ]
+    },
+    {
+        routeName: 'P5',
+        stops:
+            [
+                { stopName: 'Novia del Mediodía', geo: [123, 123], seenInfo: 1 },
+                { stopName: 'Bar Madera', geo: [123, 123], seenInfo: 0 },
+                { stopName: 'Iglesia Arroyo Arenas', geo: [123, 123], seenInfo: 0.5 },
+            ]
+    },
+    {
+        routeName: 'P6',
+        stops:
+            [
+                { stopName: 'Novia del Mediodía', geo: [123, 123], seenInfo: 1 },
+                { stopName: 'Bar Madera', geo: [123, 123], seenInfo: 0 },
+                { stopName: 'Iglesia Arroyo Arenas', geo: [123, 123], seenInfo: 0.5 },
+            ]
+    },
+    {
+        routeName: 'P7',
+        stops:
+            [
+                { stopName: 'Novia del Mediodía', geo: [123, 123], seenInfo: 1 },
+                { stopName: 'Bar Madera', geo: [123, 123], seenInfo: 0 },
+                { stopName: 'Iglesia Arroyo Arenas', geo: [123, 123], seenInfo: 0.5 },
+            ]
+    },
+    {
+        routeName: 'P8',
+        stops:
+            [
+                { stopName: 'Novia del Mediodía', geo: [123, 123], seenInfo: 1 },
+                { stopName: 'Bar Madera', geo: [123, 123], seenInfo: 0 },
+                { stopName: 'Iglesia Arroyo Arenas', geo: [123, 123], seenInfo: 0.5 },
+            ]
+    },
+    {
+        routeName: 'P9',
+        stops:
+            [
+                { stopName: 'Novia del Mediodía', geo: [123, 123], seenInfo: 1 },
+                { stopName: 'Bar Madera', geo: [123, 123], seenInfo: 0 },
+                { stopName: 'Iglesia Arroyo Arenas', geo: [123, 123], seenInfo: 0.5 },
+            ]
+    },
+    {
+        routeName: 'P10',
+        stops:
+            [
+                { stopName: 'Novia del Mediodía', geo: [123, 123], seenInfo: 1 },
+                { stopName: 'Bar Madera', geo: [123, 123], seenInfo: 0 },
+                { stopName: 'Iglesia Arroyo Arenas', geo: [123, 123], seenInfo: 0.5 },
+            ]
+    },
+    {
+        routeName: 'P11',
+        stops:
+            [
+                { stopName: 'Novia del Mediodía', geo: [123, 123], seenInfo: 1 },
+                { stopName: 'Bar Madera', geo: [123, 123], seenInfo: 0 },
+                { stopName: 'Iglesia Arroyo Arenas', geo: [123, 123], seenInfo: 0.5 },
+            ]
+    },
+    {
+        routeName: 'P12',
+        stops:
+            [
+                { stopName: 'Novia del Mediodía', geo: [123, 123], seenInfo: 1 },
+                { stopName: 'Bar Madera', geo: [123, 123], seenInfo: 0 },
+                { stopName: 'Iglesia Arroyo Arenas', geo: [123, 123], seenInfo: 0.5 },
+            ]
+    },
+    {
+        routeName: 'P13',
+        stops:
+            [
+                { stopName: 'Novia del Mediodía', geo: [123, 123], seenInfo: 1 },
+                { stopName: 'Bar Madera', geo: [123, 123], seenInfo: 0 },
+                { stopName: 'Iglesia Arroyo Arenas', geo: [123, 123], seenInfo: 0.5 },
+            ]
+    },
+    {
+        routeName: 'P14',
+        stops:
+            [
+                { stopName: 'Novia del Mediodía', geo: [123, 123], seenInfo: 1 },
+                { stopName: 'Bar Madera', geo: [123, 123], seenInfo: 0 },
+                { stopName: 'Iglesia Arroyo Arenas', geo: [123, 123], seenInfo: 0.5 },
+            ]
+    },
+    {
+        routeName: 'P15',
+        stops:
+            [
+                { stopName: 'Novia del Mediodía', geo: [123, 123], seenInfo: 1 },
+                { stopName: 'Bar Madera', geo: [123, 123], seenInfo: 0 },
+                { stopName: 'Iglesia Arroyo Arenas', geo: [123, 123], seenInfo: 0.5 },
+            ]
+    },
+    {
+        routeName: 'P16',
+        stops:
+            [
+                { stopName: 'Novia del Mediodía', geo: [123, 123], seenInfo: 1 },
+                { stopName: 'Bar Madera', geo: [123, 123], seenInfo: 0 },
+                { stopName: 'Iglesia Arroyo Arenas', geo: [123, 123], seenInfo: 0.5 },
+            ]
+    },
+    {
+        routeName: 'A40',
+        stops:
+            [
+                { stopName: 'Novia del Mediodía', geo: [123, 123], seenInfo: 1 },
+                { stopName: 'Bar Madera', geo: [123, 123], seenInfo: 0 },
+                { stopName: 'Iglesia Arroyo Arenas', geo: [123, 123], seenInfo: 0.5 },
+            ]
+    },
+    {
+        routeName: '450',
+        stops:
+            [
+                { stopName: '12 plantas', geo: [123, 123], seenInfo: 1 },
+                { stopName: '18 plantas', geo: [123, 123], seenInfo: 0 },
+                { stopName: '12 plantas Los Cocos', geo: [123, 123], seenInfo: 0.5 },
+                { stopName: 'Plaza de África', geo: [123, 123], seenInfo: 0 },
+
+            ]
+    },
+];
